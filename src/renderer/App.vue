763c432f5d0c520923bcbfaccc476716720e5b6d
amedora <americandragsterracing@gmail.com>
2018-02-28 14:51:59 +0900

    vue init simulatedgreg/electron-vue .

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-vuetify'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
