3b801da209f7912f499cbff2fc54e1b489fc5649
amedora <americandragsterracing@gmail.com>
2018-02-28 15:19:51 +0900

    vuetify ready

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,40 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <v-app>
+      <v-navigation-drawer v-model="drawer"
+        app
+        fixed
+        :clipped-left="$vuetify.breakpoint.mdAndUp"
+        />
+      <v-toolbar app dense fixed :clipped-left="$vuetify.breakpoint.mdAndUp">
+        <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+          <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+          <span class="hidden-sm-and-down">App Name</span>
+        </v-toolbar-title>
+      </v-toolbar>
+
+      <v-content>
+        <v-container fluid>
+          <router-view />
+        </v-container>
+      </v-content>
+      <v-footer app />
+    </v-app>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'electron-vuetify'
+    name: 'electron-vuetify',
+    data () {
+      return {
+        drawer: null
+      }
+    }
   }
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons');
   /* CSS */
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
 import axios from 'axios'
 
 import App from './App'
 import router from './router'
 import store from './store'
 
+import 'material-design-icons/iconfont/material-icons.css'
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
